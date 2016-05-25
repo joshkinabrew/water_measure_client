7da632d53567d1938b0f2bc8ed9ee618baea9c11
Josh Kinabrew <josh@agworld.co>
2016-05-25 10:53:26 -0600

    Add feature to save to a backup file if the RPi loses connection with the server

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # Water Measure Client
 
-This was meant as a project using a MaxBotix sensor to measure an underground cistern at an interval to ensure the water level was at an appropriate level. This client does an HTTP POST to a server to send the reading.
+This was created from a project I worked on using a MaxBotix sensor to measure an underground cistern at a specified interval to ensure the water level was at an appropriate level. This client does an HTTP POST to a server to send the reading.
 
-### Compiling for Raspberry Pi
-It should be as simple as `GOOS=linux GOARCH=arm GOARM=7 go build`
+## Features
 
-### How to use
-Once this is built and the binary copied to the Raspberry Pi, use like so:
+* If the reading is not successfully sent to the server, it saves in a backup JSON file. The next time it is able to send a reading to the server it retrieves the backup file and sends each of the readings in the JSON file. This is helpful for when an internet connection on the Raspberry Pi goes down temporarily and you still want to save the readings.
+* The client knows the correct format of a reading value and makes sure to not send an incorrect value to the server. It will retry up to 5 times to get a correct reading before failing to get the reading.
 
-`./water_measure_client "https://my-server.mydomain.com/telemetry/endpoint.json?auth=token"`
+## Compiling for use on Raspberry Pi
+It is as simple as running `GOOS=linux GOARCH=arm GOARM=7 go build`
+
+## How to use
+Once this project is built as per instructions above and the binary file is copied to the Raspberry Pi, use like so:
+
+`./water_measure_client https://my-server.mydomain.com/telemetry/endpoint.json?auth=token`
+
+Additionally you may add it in a Cron job to run at a specified interval like so:
+
+`*/15 * * * * pi /home/pi/water_measure https://my-server.mydomain.com/telemetry/endpoint.json?auth=token >> /home/pi/water_measure.log 2>&1`
+
+(This cron job example runs every 15 minutes but feel free to customize it to your liking)
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
@@ -15,8 +17,13 @@ import (
 )
 
 var (
-	url = kingpin.Arg("server", "URL of server to POST results to").Required().String()
+	serverURL      = kingpin.Arg("server", "URL of server to POST results to").Required().String()
+	fileDir        = "/home/pi/backup_readings/"
+	backupFileName = "backup_readings.json"
+	seq            = Readings{}
 )
+
+type Readings []Reading
 
 type Reading struct {
 	Value string `json:"value"`
@@ -27,41 +34,186 @@ type Reading struct {
 func main() {
 	kingpin.Parse()
 
+	r := Reading{}
+	r.setHostname()
+	r.setReadingTime()
+	r.setValueFromSerial()
+
+	status := r.sendJSONToServer()
+	log.Printf("Status Code: %v", status)
+
+	if strings.HasPrefix(status, "200") {
+		log.Print("Successfully POSTed reading to server")
+		hasBackupReadings, backupReadings := r.hasBackupReadingsInJSONFile()
+		if hasBackupReadings {
+			log.Print(fmt.Sprint("There are ", len(backupReadings), " readings in backup JSON file"))
+			sendBackupReadingsToServer(backupReadings)
+		}
+	} else {
+		log.Print("Couldn't POST to server, saving in backup file to try again later")
+		r.writeReadingToFile(fileDir)
+	}
+}
+
+func sendBackupReadingsToServer(backupReadings Readings) {
+	for index, reading := range backupReadings {
+		log.Print(fmt.Sprint("Sending Reading ", index+1, " of ", len(backupReadings)))
+		status := reading.sendJSONToServer()
+
+		if strings.HasPrefix(status, "200") {
+			log.Print("Successfully sent reading to server")
+			reading.removeReadingFromBackupFile()
+		}
+
+	}
+
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func writeJSONToFile(json []byte) error {
+	path := fmt.Sprint(fileDir, backupFileName)
+	werr := ioutil.WriteFile(path, json, os.ModePerm)
+
+	return werr
+}
+
+func readSerialValue() (value string) {
 	c := &serial.Config{Name: "/dev/ttyAMA0", Baud: 9600}
 	s, err := serial.OpenPort(c)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 
 	buf := make([]byte, 128)
 	n, err := s.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	str := strings.Trim(string(buf[:n]), "\r")
+	check(err)
 
+	return strings.Trim(string(buf[:n]), "\r")
+}
+
+func (readings Readings) pos(reading Reading) int {
+	for index, v := range readings {
+		if v.Time == reading.Time {
+			return index
+		}
+	}
+	return -1
+}
+
+func (r *Reading) removeReadingFromBackupFile() {
+	path := fmt.Sprint(fileDir, backupFileName)
+	readingsFromJSONFile := Readings{}
+
+	file, e := ioutil.ReadFile(path)
+	check(e)
+
+	dec := json.NewDecoder(strings.NewReader(string(file)))
+	decErr := dec.Decode(&readingsFromJSONFile)
+	check(decErr)
+
+	indexOfReading := readingsFromJSONFile.pos(*r)
+
+	// Remove the current reading from the slice of readings in the file
+	readingsFromJSONFile = append(readingsFromJSONFile[:indexOfReading], readingsFromJSONFile[indexOfReading+1:]...)
+
+	j, err := json.Marshal(readingsFromJSONFile)
+	check(err)
+
+	log.Print("Removing reading from backup file...")
+	werr := writeJSONToFile(j)
+	check(werr)
+}
+
+func (r *Reading) hasBackupReadingsInJSONFile() (bool, Readings) {
+	readingsFromJSONFile := Readings{}
+	path := fmt.Sprint(fileDir, backupFileName)
+	file, e := ioutil.ReadFile(path)
+	check(e)
+	dec := json.NewDecoder(strings.NewReader(string(file)))
+	decErr := dec.Decode(&readingsFromJSONFile)
+	check(decErr)
+
+	if len(readingsFromJSONFile) > 0 {
+		return true, readingsFromJSONFile
+	} else {
+		return false, readingsFromJSONFile
+	}
+}
+
+func (r *Reading) writeReadingToFile(dir string) {
+	if _, err := os.Stat(dir); err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir(dir, 0755)
+		} else {
+			log.Println(err)
+		}
+	}
+
+	path := fmt.Sprint(dir, backupFileName)
+
+	file, e := ioutil.ReadFile(path)
+	check(e)
+
+	dec := json.NewDecoder(strings.NewReader(string(file)))
+	decErr := dec.Decode(&seq)
+	check(decErr)
+
+	seq = append(seq, *r)
+
+	j, err := json.Marshal(seq)
+	check(err)
+
+	werr := writeJSONToFile(j)
+	check(werr)
+}
+
+func (r *Reading) setReadingTime() {
+	r.Time = time.Now().Unix()
+}
+
+func (r *Reading) setHostname() {
 	h, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
-	r := Reading{Value: str, Time: time.Now().Unix(), Host: h}
+	check(err)
+	r.Host = h
+}
 
+func (r *Reading) setValueFromSerial() {
+	var success bool
+	var retries int
+
+	// Retry up to 5 times getting a correct reading from the serial pin
+	for success != true && retries < 4 {
+		retries++
+		val := readSerialValue()
+		// A correct reading should always look like this: 'R0569'
+		// i.e. an "R", and 4 ints after the "R"
+		match, _ := regexp.MatchString("^R[0-9]{4}", val)
+		if match && len(val) == 5 {
+			success = true
+			r.Value = val
+		} else {
+			log.Print("Couldn't get a correct reading from sensor, retrying...")
+		}
+	}
+
+	if success != true {
+		panic("Was unable to get a correct reading from sensor")
+	}
+}
+
+func (r *Reading) sendJSONToServer() (status string) {
 	b, err := json.Marshal(r)
 
-	req, err := http.NewRequest("POST", *url, bytes.NewBuffer(b))
+	req, err := http.NewRequest("POST", *serverURL, bytes.NewBuffer(b))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	// body, _ := ioutil.ReadAll(resp.Body)
-
-	//
-
+	return resp.Status
 }
